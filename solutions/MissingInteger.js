5bd54178036360994108d308a537f9f6ff65459f
Shani <shanigeva12@gmail.com>
2023-07-02 20:30:11 +0300

    another try

--- solutions/MissingInteger.js
+++ solutions/MissingInteger.js
@@ -1,13 +1,8 @@
 function solution(A) {
-  const sortSet = new Set(A.sort());
-  const sorted = [...sortSet];
-  const length = sorted.length;
-  if (sorted[length - 1] < 0) return 1;
-  for (let i = 0; i < length - 1; i++) {
-    if (sorted[i] > 0 && sorted[i] + 1 !== sorted[i + 1]) return sorted[i] + 1;
-  }
-
-  if (sortSet.has(sorted[length - 1] - 1) || sorted[length - 1] - 1 <= 0)
-    return sorted[length - 1] + 1;
-  return sorted[length - 1] - 1;
+  const sorted = A.sort();
+  let minVal = 1;
+  sorted.forEach((item) => {
+    if (item === minVal) minVal++;
+  });
+  return minVal;
 }
