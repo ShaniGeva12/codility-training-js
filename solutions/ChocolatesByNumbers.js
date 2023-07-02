579fa12fe09d7abb91e798923cf56f0d4ea87da0
Shani <shanigeva12@gmail.com>
2023-07-02 21:28:15 +0300

    greatest common divisor

--- solutions/ChocolatesByNumbers.js
+++ solutions/ChocolatesByNumbers.js
@@ -1,14 +1,13 @@
 function solution(N, M) {
-  const eat = new Set();
-  let position = 0;
-  while (!eat.has(position)) {
-    eat.add(position);
-
-    position = position + M;
-    if (position >= N) {
-      position = position - N;
-    }
-  }
-
-  return eat.size;
+  return N / gcdByDivision(N, M);
 }
+
+function gcdByDivision(A, B) {
+  if (A % B == 0) return B;
+  else return gcdByDivision(B, A % B);
+}
+
+/*
+The problem is to count the number of chocolates you will eat when there are N chocolates in a circle.
+The strategy is to divide N by the greatest common divisor of N and M.
+*/
