1183114355cf84f352d109985f886013413d1874
Shani <shanigeva12@gmail.com>
2023-07-02 20:50:37 +0300

    100% solution | O(N) / O(Nlog(N))

--- solutions/MissingInteger.js
+++ solutions/MissingInteger.js
@@ -1,8 +1,19 @@
 function solution(A) {
-  const sorted = A.sort();
-  let minVal = 1;
-  sorted.forEach((item) => {
-    if (item === minVal) minVal++;
+  const isNumbers = new Array(A.length + 1).fill(false);
+  let i = 1;
+  if (A.length === 0 || (A.length === 1 && (A[0] > 1 || A[0] <= 0))) {
+    return 1;
+  }
+
+  A.forEach((item) => {
+    if (item > 0 && item < isNumbers.length) isNumbers[item] = true;
   });
-  return minVal;
+
+  while (i < isNumbers.length) {
+    if (isNumbers[i] === false) {
+      return i;
+    } else i++;
+  }
+
+  return i;
 }
