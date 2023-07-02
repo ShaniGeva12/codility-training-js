dc2a2a2a8093114bcb5d7891b5dc708e6fa3d38d
Shani <shanigeva12@gmail.com>
2023-07-02 20:25:10 +0300

    second try

--- solutions/MissingInteger.js
+++ solutions/MissingInteger.js
@@ -1,8 +1,13 @@
 function solution(A) {
-  const sorted = [...new Set(A.sort())];
-  if (sorted[sorted.length - 1] < 0) return 1;
-  for (let i = 0; i < sorted.length - 1; i++) {
+  const sortSet = new Set(A.sort());
+  const sorted = [...sortSet];
+  const length = sorted.length;
+  if (sorted[length - 1] < 0) return 1;
+  for (let i = 0; i < length - 1; i++) {
     if (sorted[i] > 0 && sorted[i] + 1 !== sorted[i + 1]) return sorted[i] + 1;
   }
-  return sorted[sorted.length - 1] + 1;
+
+  if (sortSet.has(sorted[length - 1] - 1) || sorted[length - 1] - 1 <= 0)
+    return sorted[length - 1] + 1;
+  return sorted[length - 1] - 1;
 }
